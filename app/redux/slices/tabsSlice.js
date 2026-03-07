7bbf7a6f28b920be1ed2ea5c441896de4f5abb15
Nipol Roy <nipolroy5@gmail.com>
2026-03-07 16:10:14 +0600

    add some element

--- app/redux/slices/tabsSlice.js
+++ app/redux/slices/tabsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const  initialState = {
-        activeTab: "profile",
+        activeTab: "dashboard",
         loading: false,
         errors: null
     }
